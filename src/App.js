23b6e025a64154a08473b0fb0abfa3897fd9c1c2
rsuubinn <rsuubinn@gmail.com>
2022-09-15 23:46:37 +0900

    #6.1 useEffect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import Button from "./Button";
 import styles from "./App.module.css";
+import { useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const onClick = () => {
+    setCounter(counter + 1);
+  };
+  console.log("I run all the time");
+  useState(() => {
+    console.log("Once");
+  }, []);
   return (
     <div>
       <h1 className={styles.title}>Welcome back to React!</h1>
-      <Button text="hello" />
+      <h3>{counter}</h3>
+      <button onClick={onClick}>Click me!</button>
     </div>
   );
 }
