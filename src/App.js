efe3a34a2cebd9b9e035626579da712b4348f657
rsuubinn <rsuubinn@gmail.com>
2022-09-16 18:43:03 +0900

    #6.4 Cleanup

--- src/App.js
+++ src/App.js
@@ -1,35 +1,23 @@
-import styles from "./App.module.css";
 import { useEffect, useState } from "react";
 
-function App() {
-  const [counter, setCounter] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => {
-    setCounter(counter + 1);
-  };
-  const onChange = (event) => {
-    setKeyword(event.target.value);
-  };
+function Hello() {
   useEffect(() => {
-    console.log("I run only once.");
+    console.log("Hi :)");
+    return () => console.log("Bye :(");
   }, []);
-  useEffect(() => {
-    console.log("I run when 'keyword' changes.");
-  }, [keyword]);
-  useEffect(() => {
-    console.log("I run when 'counter' changes.");
-  }, [counter]);
+  return <h1>Hello</h1>;
+}
+
+function App() {
+  const [showing, setShowing] = useState(false);
+
+  const onClick = () => {
+    setShowing((prev) => !prev);
+  };
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        placeholder="Searching for .."
-        type="text"
-      ></input>
-      <h1 className={styles.title}>Welcome back to React!</h1>
-      <h3>{counter}</h3>
-      <button onClick={onClick}>Click me!</button>
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      {showing ? <Hello></Hello> : null}
     </div>
   );
 }
