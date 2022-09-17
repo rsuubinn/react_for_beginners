6e64daf2e322dc9d3a0cb7e0a3ced9384ee302b0
rsuubinn <rsuubinn@gmail.com>
2022-09-17 23:24:37 +0900

    #7.2 Coin Tracker

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { useEffect, useState } from "react";
 function App() {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
+  const [coinPrice, setCoinPrice] = useState(1);
+  const [dollars, setDollars] = useState(1);
+  const [showing, setShowing] = useState(false);
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
       .then((response) => response.json())
@@ -11,35 +14,34 @@ function App() {
         setLoading(false);
       });
   }, []);
-  const [dollars, setDollars] = useState(0);
+
   const onChange = (event) => {
-    setDollars(event.target.value);
-    console.log(dollars);
+    setCoinPrice(event.target.value);
   };
-  const [counter, setCounter] = useState(0);
+  const onNeed = (event) => {
+    setDollars(event.target.value);
+    setShowing(true);
+  };
   return (
     <div>
-      <h1>The Coins! {loading ? null : `(${coins.length})`} </h1>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`} </h1>
       {loading ? (
         <h3>Loading ...</h3>
       ) : (
         <div>
-          <select>
+          <select onChange={onChange}>
+            <option>Select coin :)</option>
             {coins.map((coin) => (
-              <option key={coin.id}>
+              <option key={coin.id} value={coin.quotes.USD.price}>
                 {coin.name} ({coin.symbol}): ${coin.quotes.USD.price}USD
               </option>
             ))}
           </select>
           <h3>내가 가지고 있는 달러</h3>
-          <input
-            value={dollars}
-            onChange={onChange}
-            placeholder="입력하세요..."
-            type="number"
-          />
-          <button>계산하기</button>
-          <h3>살 수 있는 코인 개수: {counter}개</h3>
+          <input onChange={onNeed} placeholder="입력하세요..." type="number" />
+          {showing ? (
+            <h3>구매 가능한 코인 개수: {Math.floor(dollars / coinPrice)}개</h3>
+          ) : null}
         </div>
       )}
     </div>
