63ef58352920cfb92cf467cb1a5f52ca377aaa17
rsuubinn <rsuubinn@gmail.com>
2022-09-17 22:27:16 +0900

    #7.2 Coin Tracker

--- src/App.js
+++ src/App.js
@@ -1,35 +1,47 @@
 import { useEffect, useState } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDos((currentArray) => [toDo, ...currentArray]);
-    setToDo("");
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+  const [dollars, setDollars] = useState(0);
+  const onChange = (event) => {
+    setDollars(event.target.value);
+    console.log(dollars);
   };
+  const [counter, setCounter] = useState(0);
   return (
     <div>
-      <h1>오늘의 할 일({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          value={toDo}
-          onChange={onChange}
-          type="text"
-          placeholder="오늘의 할 일"
-        />
-        <button>추가</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+      <h1>The Coins! {loading ? null : `(${coins.length})`} </h1>
+      {loading ? (
+        <h3>Loading ...</h3>
+      ) : (
+        <div>
+          <select>
+            {coins.map((coin) => (
+              <option key={coin.id}>
+                {coin.name} ({coin.symbol}): ${coin.quotes.USD.price}USD
+              </option>
+            ))}
+          </select>
+          <h3>내가 가지고 있는 달러</h3>
+          <input
+            value={dollars}
+            onChange={onChange}
+            placeholder="입력하세요..."
+            type="number"
+          />
+          <button>계산하기</button>
+          <h3>살 수 있는 코인 개수: {counter}개</h3>
+        </div>
+      )}
     </div>
   );
 }
