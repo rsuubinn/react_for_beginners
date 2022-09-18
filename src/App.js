4c51fc6334d9483ce5490ba3472e143f0ee563d6
rsuubinn <rsuubinn@gmail.com>
2022-09-18 18:39:21 +0900

    #7.3 Movie App part One

--- src/App.js
+++ src/App.js
@@ -2,47 +2,36 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [coinPrice, setCoinPrice] = useState(1);
-  const [dollars, setDollars] = useState(1);
-  const [showing, setShowing] = useState(false);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const response = await fetch(
+      `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
+    );
+    const json = await response.json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
+    getMovies();
   }, []);
-
-  const onChange = (event) => {
-    setCoinPrice(event.target.value);
-  };
-  const onNeed = (event) => {
-    setDollars(event.target.value);
-    setShowing(true);
-  };
+  console.log(movies);
   return (
     <div>
-      <h1>The Coins! {loading ? "" : `(${coins.length})`} </h1>
       {loading ? (
-        <h3>Loading ...</h3>
+        <h1>Loading...</h1>
       ) : (
-        <div>
-          <select onChange={onChange}>
-            <option>Select coin :)</option>
-            {coins.map((coin) => (
-              <option key={coin.id} value={coin.quotes.USD.price}>
-                {coin.name} ({coin.symbol}): ${coin.quotes.USD.price}USD
-              </option>
-            ))}
-          </select>
-          <h3>내가 가지고 있는 달러</h3>
-          <input onChange={onNeed} placeholder="입력하세요..." type="number" />
-          {showing ? (
-            <h3>구매 가능한 코인 개수: {Math.floor(dollars / coinPrice)}개</h3>
-          ) : null}
-        </div>
+        movies.map((movie) => (
+          <div key={movie.id}>
+            <img src={movie.medium_cover_image}></img>
+            <h3>{movie.title}</h3>
+            <p>{movie.summary}</p>
+            <ul>
+              {movie.hasOwnProperty("genres")
+                ? movie.genres.map((genre) => <li key={genre}>{genre}</li>)
+                : null}
+            </ul>
+          </div>
+        ))
       )}
     </div>
   );
