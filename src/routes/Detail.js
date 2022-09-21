0183625cfa487f4297366565809e04c23281e27d
rsuubinn <rsuubinn@gmail.com>
2022-09-21 23:25:15 +0900

    #7.9 Styles

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import styles from "./Detail.module.css";
 
 function Detail() {
   const [movie, setMovie] = useState({});
+  const [loading, setLoading] = useState(true);
   const { id } = useParams();
 
   useEffect(() => {
@@ -12,18 +14,52 @@ function Detail() {
       ).json();
       console.log(json.data.movie);
       setMovie(json.data.movie);
+      setLoading(false);
     };
     getMovie();
   }, [id]);
 
   return (
     <div>
-      <img src={movie.background_image} alt={movie.title} />
-      <img src={movie.small_cover_image} alt={movie.title} />
-      <h1>{movie.title}</h1>
-      <p>{movie.description_full}</p>
-      <span>{movie.rating}</span>
-      <span>{movie.year}</span>
+      {loading ? (
+        <div className={styles.loader}>
+          <span>Loading...</span>
+        </div>
+      ) : (
+        <div className={styles.detail}>
+          <img
+            className={styles.detail__backgroundImg}
+            src={movie.background_image}
+            alt={movie.title}
+          />
+          <div className={styles.detail__components}>
+            <img
+              className={styles.detail__poster}
+              src={movie.medium_cover_image}
+              alt={movie.title}
+            />
+            <h1 className={styles.detail__title}>{movie.title}</h1>
+            <span className={styles.detail__rating}>평점 {movie.rating}</span>
+            {movie.runtime / 60 >= 1 ? (
+              <span className={styles.detail__time}>
+                {Math.floor(movie.runtime / 60)}시간 {movie.runtime % 60}분
+              </span>
+            ) : (
+              <span className={styles.detail__time}>{movie.runtime}분</span>
+            )}
+
+            <span className={styles.detail__year}>{movie.year}</span>
+            <ul className={styles.detail__genres}>
+              {movie.genres.map((genre) => (
+                <li key={movie.id}>{genre}</li>
+              ))}
+            </ul>
+            <p className={styles.detail__description}>
+              {movie.description_full}
+            </p>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
